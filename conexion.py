84252c35eab756929e57c7509ca6eb511c778cf3
masterdata-lab <masterdata@gruposima.ar>
2026-07-24 16:53:06 -0300

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -10,7 +10,7 @@ def conectar_google():
         # Cargar el nuevo token de usuario desde los secretos de Streamlit
         token_json = json.loads(st.secrets["google_token_json"])
         
-        # Crear credenciales usando el Refresh Token (para que no caduque)
+        # Crear credenciales usando el Refresh Token
         creds = Credentials.from_authorized_user_info(token_json)
         
         # Conectar a Drive y a Sheets en tu nombre
@@ -21,22 +21,19 @@ def conectar_google():
     except Exception as e:
         st.error(f"Error conectando a Google: {e}")
         return None, None
-        
+
 def obtener_o_crear_carpeta(drive_service, nombre_carpeta, parent_id):
     """
     Busca una carpeta por nombre dentro de un parent_id. 
     Si no existe, la crea y devuelve el nuevo ID.
     """
-    # 1. Buscar si la carpeta ya existe
     query = f"mimeType='application/vnd.google-apps.folder' and name='{nombre_carpeta}' and '{parent_id}' in parents and trashed=false"
     resultados = drive_service.files().list(q=query, spaces='drive', fields='files(id, name)').execute()
     carpetas = resultados.get('files', [])
 
     if carpetas:
-        # Si existe, devolvemos el ID de la primera que encuentre
         return carpetas[0].get('id')
     else:
-        # 2. Si no existe, la creamos
         metadata = {
             'name': nombre_carpeta,
             'mimeType': 'application/vnd.google-apps.folder',
